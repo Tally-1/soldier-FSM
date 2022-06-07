6b74d85ddba4d7587c1953754154362d4317ecc7
Tally-1 <86693523+Tally-1@users.noreply.github.com>
2022-06-07 18:54:11 -0300

    indentation fix

--- soldierFSM/cfgfunctions.hpp
+++ soldierFSM/cfgfunctions.hpp
@@ -4,9 +4,9 @@ class CfgFunctions
 	{
 		class misc
 		{
-			class HandleSuppression {file = "\soldierFSM\functions\misc\fn_HandleSuppression.sqf"};
+			class HandleSuppression 	{file = "\soldierFSM\functions\misc\fn_HandleSuppression.sqf"};
 			class TaskManager 		{file = "\soldierFSM\functions\misc\fn_TaskManager.sqf"};
-			class StoreSuppression	{file = "\soldierFSM\functions\misc\fn_StoreSuppression.sqf"};
+			class StoreSuppression		{file = "\soldierFSM\functions\misc\fn_StoreSuppression.sqf"};
 			class InitMan			{file = "\soldierFSM\functions\misc\fn_InitMan.sqf"};
 			class onSuppression		{file = "\soldierFSM\functions\misc\fn_onSuppression.sqf"};
 			class SuppressedEH		{file = "\soldierFSM\functions\misc\fn_SuppressedEH.sqf"};
@@ -22,7 +22,7 @@ class CfgFunctions
 		{
 			class dodgeEnded		{file = "\soldierFSM\functions\dodging\fn_dodgeEnded.sqf"};
 			class EndDodge			{file = "\soldierFSM\functions\dodging\fn_EndDodge.sqf"};
-			class Dodge				{file = "\soldierFSM\functions\dodging\fn_Dodge.sqf"};
+			class Dodge			{file = "\soldierFSM\functions\dodging\fn_Dodge.sqf"};
 			class GetDodgePos		{file = "\soldierFSM\functions\dodging\fn_GetDodgePos.sqf"};
 			class canDodge			{file = "\soldierFSM\functions\dodging\fn_canDodge.sqf"};
 		};
@@ -32,8 +32,8 @@ class CfgFunctions
 			class canFlinch			{file = "\soldierFSM\functions\flinching\fn_canFlinch.sqf"};
 			class endFlinch			{file = "\soldierFSM\functions\flinching\fn_endFlinch.sqf"};
 			class flinch 			{file = "\soldierFSM\functions\flinching\fn_flinch.sqf"};
-			class BigBulletFlinch	{file = "\soldierFSM\functions\flinching\fn_BigBulletFlinch.sqf"};
-			class standardFlinch	{file = "\soldierFSM\functions\flinching\fn_standardFlinch.sqf"};
+			class BigBulletFlinch		{file = "\soldierFSM\functions\flinching\fn_BigBulletFlinch.sqf"};
+			class standardFlinch		{file = "\soldierFSM\functions\flinching\fn_standardFlinch.sqf"};
 			
 		};
 
